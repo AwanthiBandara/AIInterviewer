6212af5f8fffc3c38a25acad5f11913b1b7f90fb
Awanthi <awanthibandara322@gmail.com>
2024-08-02 14:05:39 +0530

    Seeker My Feed Job Card Data Mapping

--- lib/views/seeker/seeker_my_feed_screen.dart
+++ lib/views/seeker/seeker_my_feed_screen.dart
@@ -211,19 +211,20 @@ class JobCardRecruiter extends StatelessWidget {
           Row(
             children: [
               Container(
-                height: 60,
-                width: 60,
-                decoration: BoxDecoration(
-                  color: greyTextColor,
-                  borderRadius: BorderRadius.circular(8),
-                  border: Border.all(color: secondaryColor.withOpacity(0.2), width: 1.2),
-                  image: const DecorationImage(
-                    image: NetworkImage(
-                        "https://penji.co/wp-content/uploads/2022/10/4.-OrSpeakIT.jpg"),
-                    fit: BoxFit.cover,
-                  ),
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                color: greyTextColor,
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(color: secondaryColor.withOpacity(0.2), width: 1.2),
+                image: DecorationImage(
+                  image: job.createdUser?.companyLogoUrl != null && job.createdUser!.companyLogoUrl!.isNotEmpty
+                      ? NetworkImage(job.createdUser!.companyLogoUrl!)
+                      : const AssetImage('assets/images/company_placeholder.png') as ImageProvider,
+                  fit: BoxFit.cover,
                 ),
               ),
+            ),
               const SizedBox(width: 12),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -235,16 +236,16 @@ class JobCardRecruiter extends StatelessWidget {
                         fontWeight: FontWeight.w500,
                         color: white),
                   ),
-                  const Text(
-                    "HR Head at ABC Pvt Ltd, UK",
+                  Text(
+                     "${job.createdUser?.companyName}, ${job.createdUser?.companyLocation}",
                     style: TextStyle(
                         fontSize: 14,
                         color: white,
                         fontWeight: FontWeight.w500,
                         letterSpacing: 1),
                   ),
-                  const Text(
-                    "Remote | USD85,000/yr - USD95,000/yr",
+                  Text(
+                     "${job.jobType} | ${job.salaryRange}",
                     style: TextStyle(
                         fontSize: 12, letterSpacing: 0.8, color: white),
                   ),
